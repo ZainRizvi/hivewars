43262c3ef3cdaa6e7d3034ca30ac6b4ef035301c
zainstein <zainstein@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-03-30 04:32:58 +0000

    skeleton code mostly done. I'm testing the repository synch mechanism right now

--- Hive.java
+++ Hive.java
@@ -3,8 +3,12 @@ package hivewars;
 import java.io.Serializable;
 
 public class Hive implements Serializable{
-
-	public Hive() {
+	
+	char Base; //base number
+	char controllingPlayer; 
+	char numMinions;
+	
+	public Hive(char baseNum, char controller, char minionCount) {
 		// TODO Auto-generated constructor stub
 	}
 
