6b21ab602552103f98cc5090ed2f4ffe32adbf47
aaron.murdoch <aaron.murdoch@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-03-31 05:59:15 +0000

    I'm testing Golden T game engine trying to learn it

--- trunk/Gui.java
+++ trunk/Gui.java
@@ -1,5 +1,45 @@
 package hivewars;
 
-public class Gui {
-	//contents depend on game engine we use
+//JFC
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+
+// GTGE
+import com.golden.gamedev.Game;
+import com.golden.gamedev.GameLoader;
+
+
+public class Gui extends Game implements Runnable {
+
+
+ /****************************************************************************/
+ /**************************** GAME SKELETON *********************************/
+ /****************************************************************************/
+
+    public void initResources() {
+    }
+
+    public void update(long elapsedTime) {
+    }
+
+    public void render(Graphics2D g) {
+    }
+
+
+ /****************************************************************************/
+ /***************************** START-POINT **********************************/
+ /****************************************************************************/
+
+    public void run(){
+        GameLoader game = new GameLoader();
+        game.setup(new Gui(), new Dimension(1280,800), true);
+        game.start();	
+    }
+    
+    public static void main(String[] args) {
+        GameLoader game = new GameLoader();
+        game.setup(new Gui(), new Dimension(1280,800), true);
+        game.start();
+    }
+
 }
