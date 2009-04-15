2ff9c1fe711aa1ad466b55b01219558919db9a20
zainstein <zainstein@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-04-15 05:40:54 +0000

    Changed GameStateController to use synchronized functions instead of semaphores

--- trunk/hivewars/GameStateController.java
+++ trunk/hivewars/GameStateController.java
@@ -13,56 +13,47 @@ public class GameStateController {
 		gameState = new GameStateData();
 	}
 	
-	public GameStateData readGameState(){
+	public synchronized GameStateData readGameState(){
 		return new GameStateData(gameState);
 	}
 	
-	public void updateGameState(GameStateData newGameState){
+	public synchronized void updateGameState(GameStateData newGameState){
 		//add mutex 
 		gameState = newGameState;
 	}
 		
-	public void addAttack(Attack attack){
+	public synchronized void addAttack(Attack attack){
 		//add mutex
-		getSemaphore();
 		//add attack
 		gameState.attacks.add(attack);
-		releaseSemaphore();
 	}
 
 	//update game state number
-	public void updateTime(short newStateNum){
+	public synchronized void updateTime(short newStateNum){
 		//add mutex
-		getSemaphore();
 		//update time
 		gameState.gameStateNum = newStateNum;
-		releaseSemaphore();
 	}
 	
 	//removes attack from game state list
-	public void attackCompleted(Attack attack){
+	public synchronized void attackCompleted(Attack attack){
 		//add mutex
-		getSemaphore();
 		//remove attack
 		gameState.attacks.remove(attack);
-		releaseSemaphore();
 	}
 
 	//for when hive status changes
 	//used to change the controlling player or number of minions
-	public void updateHive(char hiveNum, GameSettings.Control controllingPlayer, char numMinions){
-		
+	public synchronized void updateHive(char hiveNum, GameSettings.Control controllingPlayer, char numMinions){
 		//add mutex
-		getSemaphore();
 		//make sure no more than hiveCapacity minions are in hive
 		Hive hive = gameState.hives.get((int) hiveNum);
 		hive.controllingPlayer = controllingPlayer;
 		hive.numMinions = numMinions;
 		gameState.hives.set((int) hiveNum, hive);
-		releaseSemaphore();
 	}
 	
-	public void ReconcileGS(GameStateData newGS){
+	public synchronized void ReconcileGS(GameStateData newGS){
 		int oldStateNum = gameState.gameStateNum;
 		
 		//forward old GameState to be at the same state number as new GS
@@ -78,26 +69,26 @@ public class GameStateController {
 		}		
 	}
 	
-	public void ReconcileNewMoves(GameStateData GS){
+	public synchronized void ReconcileNewMoves(GameStateData GS){
 		GameStateData newGS = new GameStateData(); 
 		//merge a game state with PlayerAttackList commands 
 	}	
 	
-	public void fastForward(int stateNum){
+	public synchronized void fastForward(int stateNum){
 		
 	}
 	
-	public void getSemaphore(){
-		try {
-			GSFree.acquire();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void releaseSemaphore(){
-		GSFree.release();
-	}
+//	public void getSemaphore(){
+//		try {
+//			GSFree.acquire();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public void releaseSemaphore(){
+//		GSFree.release();
+//	}
 
 }
