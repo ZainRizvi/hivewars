e81ec81e30bd032b3b76e5ea7d62e1e603601ff1
zainstein <zainstein@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-04-15 03:53:37 +0000

    Created semaphore for GameStateController and wrote code to add attacks

--- hivewars/GameStateController.java
+++ hivewars/GameStateController.java
@@ -1,13 +1,16 @@
 package hivewars;
 
+import java.util.concurrent.Semaphore;
+
 
 
 public class GameStateController {
 	
 	private GameStateData gameState;
+	private static final Semaphore GSFree = new Semaphore(1, true);
 
 	public GameStateController() {
-		// TODO Auto-generated constructor stub
+		gameState = new GameStateData();
 	}
 	
 	public GameStateData readGameState(){
@@ -16,16 +19,38 @@ public class GameStateController {
 	
 	public void addAttack(Attack attack){
 		//add mutex
+		try {
+			GSFree.acquire();} catch (InterruptedException e) {e.printStackTrace();
+		}
+		//add attack
+		gameState.attacks.add(attack);
+		GSFree.release();
 	}
 
 	//update game state number
-	public void updateTime(short time){
+	public void updateTime(short newStateNum){
 		//add mutex
+		try {
+			GSFree.acquire();} catch (InterruptedException e) {e.printStackTrace();
+		}
+		//update time
+		gameState.gameStateNum = newStateNum;
+		GSFree.release();
 	}
 	
 	//removes attack from game state list
 	public void attackCompleted(Attack attack){
 		//add mutex
+		try {
+			GSFree.acquire();} catch (InterruptedException e) {e.printStackTrace();
+		}
+		//get all attacks
+		int numAttacks = gameState.attacks.size();
+		for(int i = 0; i < numAttacks; i++){
+			
+		}
+		gameState.attacks.remove(attack);
+		GSFree.release();
 	}
 
 	//for when hive status changes
@@ -38,6 +63,19 @@ public class GameStateController {
 	public void updateGameState(GameStateData newGameState){
 		//add mutex 
 		gameState = newGameState;
+	}
+	
+	public void getSemaphore(){
+		try {
+			GSFree.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void releaseSemaphore(){
+		GSFree.release();
 	}
 
 }
