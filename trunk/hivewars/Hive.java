c591acd5ef6b4592d9780fa4d8096be28a957d0c
joshbruno@gmail.com <joshbruno@gmail.com@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-04-15 05:23:12 +0000

    Dev Snapshot

--- trunk/hivewars/Hive.java
+++ trunk/hivewars/Hive.java
@@ -5,40 +5,42 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Hive implements Serializable{
 	
-	char Base; 				//base number
-	GameSettings.Control controllingPlayer; 
-	char numMinions; 		//max 250 allowed in a hive	 
-	int spawnTime;			//game state time when next minion will be spawned
+	// Defined in HiveDefinition
+	public final int Base;				//base number
+	public final GameSettings.Control startingPlayer;
+	public final int startingMinions;
+	public final int spawnRate;   		//time (in 100 miliseconds) before new minion is generated 
+	public final int hiveCapacity; 		//max number of minions this hive can contain
+	public final int x;
+	public final int y;
 
-	// Defined by Map
-	int spawnRate;   		//time (in 100 miliseconds) before new minion is generated 
-	int hiveCapacity; 		//max number of minions this hive can contain
+	// Variable
+	GameSettings.Control controllingPlayer; 
+	char numMinions; 
+	int spawnTime;			//game state time when next minion will be spawned
 	
 	
 	public Hive(
-			char baseNum, 
-			GameSettings.Control controller, 
-			char minionCount, 
-			int spawningRate, 
-			int capacity) {
-		Base = baseNum;
-		controllingPlayer = controller;
-		numMinions = minionCount;
-		spawnRate = spawningRate;
-		hiveCapacity = capacity;
+			int id, 
+			GameSettings.Control startingPlayer, 
+			int startingMinions, 
+			int spawnRate, 
+			int maxSize, 
+			int x, 
+			int y){
+		
+		this.Base = id;
+		this.startingPlayer = startingPlayer;
+		this.startingMinions = startingMinions;
+		this.spawnRate = spawnRate;
+		this.hiveCapacity = maxSize;
+		this.x = x;
+		this.y = y;
+		
 		//TODO: properly implement spawnTime initialization
 		//spawnTime = (current state num) + spawnRate; 
 	}
 	
-	// Default hive stats (this constructor is for debugging purposes only)
-	public Hive(char baseNum){
-		Base = baseNum;
-		controllingPlayer = GameSettings.Control.Neutral;
-		numMinions = 10;
-		spawnRate = 10;
-		hiveCapacity = 20;
-		//TODO: properly implement spawnTime initialization
-		//spawnTime = (current state num) + spawnRate;
-	}
+
 
 }
--- trunk/hivewars/Map.java
+++ trunk/hivewars/Map.java
@@ -4,19 +4,57 @@ import java.util.ArrayList;
 
 public class Map {
 	
-	public ArrayList<HiveDefinition> hives;
+	public static ArrayList<Hive> hives = new ArrayList<Hive>();
+	public static int[][] distances;
 	
 
 	public Map() {
-		hives.add(new HiveDefinition(0, 1, 1, 1, 1));
-		hives.add(new HiveDefinition(0, 1, 1, 1, 100));
-		hives.add(new HiveDefinition(0, 1, 1, 50, 50));		
-		
+		hives.add(new Hive( 0, GameSettings.Control.PlayerA, 1, 1, 250, 0, 0));
+		hives.add(new Hive(1, GameSettings.Control.PlayerB, 1, 1, 250, 0, 100));
+		hives.add(new Hive(2, GameSettings.Control.Neutral, 1, 1, 50, 50, 50));		
 		calculateDistances();		
 	}
 
 	
 	private void calculateDistances(){
+		distances = new int[hives.size()][hives.size()];
 		
+		for (int i = 0; i < hives.size(); i++){
+			for (int j = 0; j < hives.size(); j++){
+				if (i==j){
+					distances[i][j] = 0;
+				}else{
+					int xi = hives.get(i).x;
+					int yi = hives.get(i).y;
+					int xj = hives.get(j).x;
+					int yj = hives.get(j).y;
+					
+					int xdist = Math.abs(xi - xj);
+					int ydist = Math.abs(yi - yj);
+					
+					int dist = (int) Math.sqrt( Math.pow(xdist, 2) + Math.pow(ydist, 2));
+					distances[i][j] = dist;
+				}
+			}
+		}		
+	}
+	
+	public void printDistances(){
+		for (int i = 0; i < distances.length; i++){
+			System.out.print("[");
+			for (int j = 0; j < distances.length; j++){
+				System.out.print(distances[i][j]);
+				if (j != distances.length - 1){
+					System.out.print("\t");
+				}
+			}
+			System.out.println("]");
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		Map map = new Map();
+		map.printDistances();
 	}
 }
