306454ea73d5c33e43d384bb4e3f4cc267c1b30f
zainstein <zainstein@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-04-15 06:05:04 +0000

    Socket information is now stored in GameController

--- hivewars/Receive.java
+++ hivewars/Receive.java
@@ -3,31 +3,44 @@ package hivewars;
 import java.net.InetAddress;
 
 public class Receive implements Runnable{
-	UDPSocket socket;
 	int localPort;
 	InetAddress localInetAddr;
 	
 	public Receive() {
-		socket = new UDPSocket();
-		localPort = socket.getLocalPort();
-		localInetAddr = socket.getLocalAddress();
+		//socket = new UDPSocket();
+		localPort = GameController.socket.getLocalPort();
+		localInetAddr = GameController.socket.getLocalAddress();
 		
 		Thread t = new Thread(this);
 		t.start();
-	}
-	
-	
+	}	
 	
 	@Override
 	public void run() {
-		//get incoming game state
-		GameStateData incomingGS = (GameStateData) socket.getMessage();
-		//store game state
-		GameController.MasterGS.ReconcileGS(incomingGS);
-		//wake up Reconcile
+		receiveGameStates();
 	}
 	
+	//
+	// Keep receiving new game states from remote player and merge them
+	//   with the master game state
+	//
+	public void receiveGameStates(){
+		//get incoming game state
+		GameStateData incomingGS = (GameStateData) GameController.socket.getMessage();
+		
+		//store remote socket's and IP address information
+		GameController.remotePort = GameController.socket.remoteSocketData.port;			
+		GameController.remoteInetAddr = GameController.socket.remoteSocketData.IPAddr;
 
-	
-
+		//reconcile incoming game state with master game state
+		GameController.MasterGS.ReconcileGS(incomingGS);
+		
+		while(!GameController.GameFinished){
+			//get incoming game state
+			incomingGS = (GameStateData) GameController.socket.getMessage();
+		
+			//reconcile incoming game state with master game state
+			GameController.MasterGS.ReconcileGS(incomingGS);			
+		}		
+	}
 }
