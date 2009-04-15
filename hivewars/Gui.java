36b639419fa4ca77eb60874f2f6458ff619bd365
aaron.murdoch <aaron.murdoch@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-04-15 06:26:05 +0000

    Gui starts a thread with the GoldenT engine in it just say "new Gui();" to start the Gui

--- hivewars/Gui.java
+++ hivewars/Gui.java
@@ -1,45 +1,33 @@
 package hivewars;
 
-//JFC
-import java.awt.Graphics2D;
 import java.awt.Dimension;
-
-// GTGE
-import com.golden.gamedev.Game;
 import com.golden.gamedev.GameLoader;
 
+//this class simply starts the Gui thread
+public class Gui extends Thread {
+	
+	//constructor
+	public Gui() {
+		start();
+	}
 
-public class Gui extends Game implements Runnable {
-
-
- /****************************************************************************/
- /**************************** GAME SKELETON *********************************/
- /****************************************************************************/
-
-    public void initResources() {
-    }
-
-    public void update(long elapsedTime) {
-    }
-
-    public void render(Graphics2D g) {
-    }
-
-
- /****************************************************************************/
- /***************************** START-POINT **********************************/
- /****************************************************************************/
-
-    public void run(){
+	public void run() {
         GameLoader game = new GameLoader();
-        game.setup(new Gui(), new Dimension(1280,800), true);
+        game.setup(new GoldenT(), new Dimension(800,600), true);
+        //start:
+        //		initResouces()
+        //		while(true){
+        //			update();
+        //			render();
+        //		}
+        //plus other GTGE background stuff
         game.start();	
-    }
-    
+	}
+	
+    //main:
+    //for testing  
     public static void main(String[] args) {
-        GameLoader game = new GameLoader();
-        game.setup(new Gui(), new Dimension(1280,800), true);
-        game.start();
+    	new Gui();
     }
 
 }
