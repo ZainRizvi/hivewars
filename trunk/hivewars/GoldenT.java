07bff060aeee49405a8f7b6bef13c2dd18488f7f
aaron.murdoch <aaron.murdoch@9cf69a54-1b28-11de-9c9a-2d325a3e6494>
2009-04-19 01:31:46 +0000

    Got the Gui and the initial part working!!!

--- trunk/hivewars/GoldenT.java
+++ trunk/hivewars/GoldenT.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.image.*;
 import java.awt.event.*;
 import java.lang.Math;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import java.awt.GraphicsEnvironment.*;
@@ -31,7 +33,7 @@ import com.golden.gamedev.gui.toolkit.*;
 public class GoldenT extends Game {
 
 	final int NUMBER_OF_HIVES;
-	SystemFont sf;
+	SystemFont sf, label, err, ann, title;
 	BufferedImage[] attck;
 	//current gs, read from GameController.ViewableGS
 	GameStateData currentGS;
@@ -42,43 +44,89 @@ public class GoldenT extends Game {
 	ArrayList<MinionNumber> minionNumbers;
 	int click;
 	int selectedHive, sourceHive, destHive;
+	char mode;
 	//sprites
 	ArrayList<AnimatedSprite> hives, attacks;
 	SpriteGroup Hives, Attacks;
 	//colisions
 	CollisionBounds outOfBounds;
 	CollisionGroup attackToHive;
+	//GUI variables
+	FrameWork frame;
+	TTextField ipField, portField;
+	TButton waitButton, connectToButton, connectButton;
+	char userError;
 	
-	TTextField t;
+	
 	
 	//constructor
 	public GoldenT() {
 		//read initial Viewable game state
 		currentGS = GameController.ViewableGS.readGameState();
 		NUMBER_OF_HIVES = currentGS.hives.size();
-		GameController.Me = GameSettings.Control.PlayerA;
+    	mode = 0;
+    	click = 0;
+    	userError = 0;
+    	oldGameStateNum = -1;
+    	selectedHive = -1;
 	}
+	
+	
 	
 	//toggles flash screen, gtge logo in top right of screen, and fps in bottom left
 	{ distribute = false; }
-	FrameWork frame;
+
+	
 	
     //initResouces
+	//this is the first function called by start(), it is only called once
 	public void initResources() {
-    	
-		t = new TTextField("the world is great", 100, 100, 200, 20) {
-			public void doAction() {
-				System.out.println("hello world");
-			}
-		};
-		t.setEditable(true);
-		
-			
-		
-		frame = new FrameWork(bsInput, 800, 600);
-		frame.add(t);
 		
 		showCursor();
+		
+		//set up gui pieces
+		frame = new FrameWork(bsInput, 800, 600);
+		ipField = new TTextField("", 350, 250, 100, 20) {
+			public void doAction() {
+				connect(ipField.getText(), portField.getText());
+			}
+		};
+		ipField.setEditable(true);
+		ipField.setMaxLength(15);
+		ipField.setEnabled(false);
+		ipField.setVisible(false);
+		frame.add(ipField);
+		portField = new TTextField("", 375, 300, 50, 20) {
+			public void doAction() {
+				connect(ipField.getText(), portField.getText());
+			}
+		};
+		portField.setEditable(true);
+		portField.setMaxLength(5);
+		portField.setEnabled(false);
+		portField.setVisible(false);
+		frame.add(portField);
+		waitButton = new TButton("Wait for opponent", 150, 200, 200, 50) {
+			public void doAction() {
+				mode = 1;
+				GameController.Me = GameSettings.Control.PlayerA;
+			}
+		};
+		frame.add(waitButton);
+		connectToButton = new TButton("Connect to ...", 450, 200, 200, 50) {
+			public void doAction() {
+				mode = 3;
+			}
+		};
+		frame.add(connectToButton);
+		connectButton = new TButton("Connect", 300, 350, 200, 50) {
+			public void doAction() {
+				connect(ipField.getText(), portField.getText());
+			}
+		};
+		connectButton.setEnabled(false);
+		connectButton.setVisible(false);
+		frame.add(connectButton);
     	
     	//make background
     	bg = new ImageBackground(getImage("SuperNova.jpg"),1024,768);
@@ -107,6 +155,14 @@ public class GoldenT extends Game {
     	//set up Font
     	Font f = new Font("Helvetica", Font.PLAIN, 30);
     	sf = new SystemFont(f, Color.WHITE);
+    	f = new Font("Helvetica", Font.PLAIN, 20);
+    	label = new SystemFont(f, Color.RED);
+    	f = new Font("Helvetica", Font.PLAIN, 30);
+    	err = new SystemFont(f, Color.YELLOW);
+    	f = new Font("Helvetica", Font.PLAIN, 30);
+    	ann = new SystemFont(f, Color.RED);
+    	f = new Font("Helvetica", Font.PLAIN, 100);
+    	title = new SystemFont(f, Color.RED);
     	
     	//create sprites and spritegroups
     	Hives= new SpriteGroup("Hives");
@@ -132,6 +188,7 @@ public class GoldenT extends Game {
     		minionNumbers.add(k, mn);
     		Hives.add(hives.get(k));
     	}
+    	Hives.setActive(false);
     	
     	attacks = new ArrayList<AnimatedSprite>();
     	Attacks = new SpriteGroup("Attacks");
@@ -147,18 +204,13 @@ public class GoldenT extends Game {
     	attackToHive = new AttackToHiveCollision();
     	attackToHive.setCollisionGroup(Attacks, Hives);
     	
-    	//start game
-    	GameController.GameStarted = true;
-    	click = 0;
-    	oldGameStateNum = -1;
-    	selectedHive = -1;
     }
 	
 	
     //this function determines the x and y velocity it takes to get to a destination x and y at a specified speed
 	//inputs: current x and y, destination x and y, and velocity
 	//output: an array of doubles {x velocity, y velocity}
-	public int[] getSpeedTo(int xOld, int yOld ,int xNew, int yNew){
+	private int[] getSpeedTo(int xOld, int yOld ,int xNew, int yNew){
 		double hyp;
 		int xv, yv, xdist, ydist;
 		int ret[] = {0, 0};
@@ -173,132 +225,245 @@ public class GoldenT extends Game {
 	}
 	
 	
+	
+	//this functions sets the remote ip and port in GameController
+	//inputs string ip and string port
+	private void connect(String ip, String port){
+		String p0 = new String();
+		String p1 = new String();
+		String p2 = new String();
+		String p3 = new String();
+		//check for valid input
+		if(ip.length() == 0 && port.length() == 0){
+			userError = 1;		//please enter an ip address and a port number
+		} else if(ip.length() == 0){
+			userError = 2;		//please enter an ip address
+		} else if(port.length() == 0){
+			userError = 3;		//please enter a port number
+		} else {
+			GameController.remotePort = Integer.parseInt(port);
+			//parse ip address from string
+			int k = 0;
+			while((ip.charAt(k) != '.') && (k < (ip.length()))){
+				p0 += ip.charAt(k);
+				k++;
+			}
+			k++;
+			while((ip.charAt(k) != '.') && (k < (ip.length()))){
+				p1 += ip.charAt(k);
+				k++;
+			}
+			k++;
+			while((ip.charAt(k) != '.') && (k < (ip.length()))){
+				p2 += ip.charAt(k);
+				k++;
+			}
+			k++;
+			while(k < (ip.length())){
+				p3 += ip.charAt(k);
+				k++;
+			}
+			byte[] addr = new byte[4];
+			addr[0] = (byte)Integer.parseInt(p0);
+			addr[1] = (byte)Integer.parseInt(p1);
+			addr[2] = (byte)Integer.parseInt(p2);
+			addr[3] = (byte)Integer.parseInt(p3);
+			try {
+				GameController.remoteInetAddr = InetAddress.getByAddress(addr);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+			GameController.Me = GameSettings.Control.PlayerB;
+			mode = 5;
+			userError = 0;
+		}
+		//*return error if cannot connect*
+	}
+	
+
+	
 	//update
+	//this function is called over and over again by the GTG engine
     public void update(long elapsedTime) {
     	//local variables
     	int startFrame;
 
-    	//update currentGS from ViewableGS
-    	currentGS = GameController.ViewableGS.readGameState();
-    	if(currentGS.gameStateNum != oldGameStateNum){
-	    	//update hives
-	    	for(int k = 0; k < hives.size(); k ++){
-	    		//update minion number
-	    		minionNumbers.get(k).setNumber(currentGS.hives.get(k).numMinions);
-	    		//update hive color
-	    		startFrame = hives.get(k).getStartAnimationFrame();
-	    		if(currentGS.hives.get(k).controllingPlayer == GameSettings.Control.PlayerA) {
-	    			hives.get(k).setAnimationFrame(0, 2);
-	    		} else if(currentGS.hives.get(k).controllingPlayer == GameSettings.Control.PlayerB) {
-	    			hives.get(k).setAnimationFrame(1, 2);
-	    		} else if(currentGS.hives.get(k).controllingPlayer == GameSettings.Control.Neutral) {
-	    			hives.get(k).setAnimationFrame(2, 2);
-	    		}
-	    		if(hives.get(k).getStartAnimationFrame() != startFrame) {
-	    			//*explosion*
+    	if(mode == 0){
+        	frame.update();
+    	} else if(mode == 1) {
+    		waitButton.dispose();
+    		connectToButton.dispose();
+    		mode = 2;
+        	frame.update();
+    	} else if(mode == 2){
+    		if(GameController.GameStarted){
+    			mode = 5;
+    		}
+        	frame.update();
+    	} else if(mode == 3) {
+    		waitButton.dispose();
+    		connectToButton.dispose();
+    		ipField.setEnabled(true);
+    		ipField.setVisible(true);
+    		portField.setEnabled(true);
+    		portField.setVisible(true);
+    		connectButton.setEnabled(true);
+    		connectButton.setVisible(true);
+    		mode = 4;
+        	frame.update();
+    	} else if (mode == 4){
+        	frame.update();
+    	} else if (mode == 5){
+    		Hives.setActive(true);
+    		mode = 6;
+    	} else { //mode ==6
+    		//System.out.println(GameController.Me);
+    		//System.out.println(GameController.GameStarted);
+	    	//update currentGS from ViewableGS
+	    	currentGS = GameController.ViewableGS.readGameState();
+	    	if(currentGS.gameStateNum != oldGameStateNum){
+		    	//update hives
+		    	for(int k = 0; k < hives.size(); k ++){
+		    		//update minion number
+		    		minionNumbers.get(k).setNumber(currentGS.hives.get(k).numMinions);
+		    		//update hive color
+		    		startFrame = hives.get(k).getStartAnimationFrame();
+		    		if(currentGS.hives.get(k).controllingPlayer == GameSettings.Control.PlayerA) {
+		    			hives.get(k).setAnimationFrame(0, 2);
+		    		} else if(currentGS.hives.get(k).controllingPlayer == GameSettings.Control.PlayerB) {
+		    			hives.get(k).setAnimationFrame(1, 2);
+		    		} else if(currentGS.hives.get(k).controllingPlayer == GameSettings.Control.Neutral) {
+		    			hives.get(k).setAnimationFrame(2, 2);
+		    		}
+		    		if(hives.get(k).getStartAnimationFrame() != startFrame) {
+		    			//*explosion*
+		    		}
+		    	}
+		    	
+		    	//update attacks
+		    	//add or subtract correct number of attack sprites to sprite group
+		    	while(currentGS.attacks.size() < attacks.size()) {
+		    		attacks.remove(attacks.size() - 1);
+		    		Attacks.removeInactiveSprites();
+		    	}
+		    	while(currentGS.attacks.size() > attacks.size()) {
+		    		AnimatedSprite a = new AnimatedSprite(attck, 100, 100);
+		    		a.setAnimationFrame(0, 0);
+		    		a.setAnimate(false);
+		    		a.setActive(true);
+		    		attacks.add(a);
+		    		Attacks.add(a);
+		    	}
+		    	for(int k = 0; k < attacks.size(); k ++) {
+		    		int sourceX = currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).x;
+		    		int sourceY = currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).y;
+		    		int[] v = getSpeedTo(sourceX, sourceY, currentGS.hives.get(currentGS.attacks.get(k).destHiveNum).x,
+		    				currentGS.hives.get(currentGS.attacks.get(k).destHiveNum).y);
+		    		//x = sourceHive.x + traveledDistance
+		    		//or x = sourceHive.x + (currentTime - firingTime) * xSpeed
+		    		int x = (currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).x + hives.get(currentGS.attacks.get(k).sourceHiveNum).getWidth() / 2) + 
+		    		(currentGS.gameStateNum - currentGS.attacks.get(k).firingTime) * v[0];
+		    		int y = (currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).y + hives.get(currentGS.attacks.get(k).sourceHiveNum).getWidth() / 2) + 
+		    		(currentGS.gameStateNum - currentGS.attacks.get(k).firingTime) * v[1];
+		    		attacks.get(k).forceX(x);
+		    		attacks.get(k).forceY(y);
+		    		//System.out.println(" k" + k + " x and y: " + x + " " + y);
+		    		//*set animations*
+		    	}
+		    	
+		    	//check collisons, boundary and sprite to sprite
+		    	attackToHive.checkCollision();
+		    	outOfBounds.checkCollision();
+	    	}
+	    	
+	    	//check for mouse event
+	    	if(bsInput.isMouseDown(MouseEvent.BUTTON1)) {
+	    		int xm = getMouseX();
+	    		int ym = getMouseY();
+	    		selectedHive = -1;
+	    		for(int k = 0; k < hives.size(); k ++){	
+					if(xm >= hives.get(k).getX() && xm <= hives.get(k).getX() + hives.get(k).getWidth() &&
+	    			   ym >= hives.get(k).getY() && ym <= hives.get(k).getY() + hives.get(k).getHeight()){
+						selectedHive = k;
+	    			}
+	    		}  	
+	    		if(selectedHive != -1) System.out.println("outside if selected: " + selectedHive + " click: " + click + " controlling player: " + currentGS.hives.get(selectedHive).controllingPlayer);
+	    		if(click == 0){
+	    			if(selectedHive == -1){
+	    				click = 0;
+	    			} else if(currentGS.hives.get(selectedHive).controllingPlayer == GameController.Me) {
+	    				click = 1;
+	    				sourceHive = selectedHive;
+	    				System.out.println("click: " + click + " sourceHive: " + sourceHive);
+	    			}
+	    		} else if (click == 2) {
+	    			if(selectedHive == -1) {
+	    				click = 1;
+	    			} else if(currentGS.hives.get(selectedHive).controllingPlayer == GameController.Me){
+	    				click = 1;
+	    				sourceHive = selectedHive;	
+	    				System.out.println("click: " + click + " sourceHive: " + sourceHive);
+	    				//*make sourceHive a special selected color*
+	    			} else { 
+	    				click = 1;
+	    				destHive = selectedHive;
+						try {
+							GameController.writeCurrentAttack(new Attack(GameController.Me, 
+									(char) sourceHive, (char) destHive, (short) currentGS.gameStateNum));
+							System.out.println("new attack: source: " + sourceHive + " dest: " + destHive + " stateNum: " + currentGS.gameStateNum);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	    			}
 	    		}
 	    	}
 	    	
-	    	//update attacks
-	    	//add or subtract correct number of attack sprites to sprite group
-	    	while(currentGS.attacks.size() < attacks.size()) {
-	    		attacks.remove(attacks.size() - 1);
-	    		Attacks.removeInactiveSprites();
+	    	if(!bsInput.isMouseDown(MouseEvent.BUTTON1)){
+	    		if(click == 1){
+	    			click = 2;
+	    		}
 	    	}
-	    	while(currentGS.attacks.size() > attacks.size()) {
-	    		AnimatedSprite a = new AnimatedSprite(attck, 100, 100);
-	    		a.setAnimationFrame(0, 0);
-	    		a.setAnimate(false);
-	    		a.setActive(true);
-	    		attacks.add(a);
-	    		Attacks.add(a);
-	    	}
-	    	for(int k = 0; k < attacks.size(); k ++) {
-	    		int sourceX = currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).x;
-	    		int sourceY = currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).y;
-	    		int[] v = getSpeedTo(sourceX, sourceY, currentGS.hives.get(currentGS.attacks.get(k).destHiveNum).x,
-	    				currentGS.hives.get(currentGS.attacks.get(k).destHiveNum).y);
-	    		//x = sourceHive.x + traveledDistance
-	    		//or x = sourceHive.x + (currentTime - firingTime) * xSpeed
-	    		int x = (currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).x + hives.get(currentGS.attacks.get(k).sourceHiveNum).getWidth() / 2) + 
-	    		(currentGS.gameStateNum - currentGS.attacks.get(k).firingTime) * v[0];
-	    		int y = (currentGS.hives.get(currentGS.attacks.get(k).sourceHiveNum).y + hives.get(currentGS.attacks.get(k).sourceHiveNum).getWidth() / 2) + 
-	    		(currentGS.gameStateNum - currentGS.attacks.get(k).firingTime) * v[1];
-	    		attacks.get(k).forceX(x);
-	    		attacks.get(k).forceY(y);
-	    		//System.out.println(" k" + k + " x and y: " + x + " " + y);
-	    		//*set animations*
-	    	}
+	    	//*tell sprites which way to face when they are walking*
 	    	
-	    	//check collisons, boundary and sprite to sprite
-	    	attackToHive.checkCollision();
-	    	outOfBounds.checkCollision();
-    	}
-    	
-    	//check for mouse event
-    	if(bsInput.isMouseDown(MouseEvent.BUTTON1)) {
-    		int xm = getMouseX();
-    		int ym = getMouseY();
-    		selectedHive = -1;
-    		for(int k = 0; k < hives.size(); k ++){	
-				if(xm >= hives.get(k).getX() && xm <= hives.get(k).getX() + hives.get(k).getWidth() &&
-    			   ym >= hives.get(k).getY() && ym <= hives.get(k).getY() + hives.get(k).getHeight()){
-					selectedHive = k;
-    			}
-    		}  	
-    		if(selectedHive != -1) System.out.println("outside if selected: " + selectedHive + " click: " + click + " controlling player: " + currentGS.hives.get(selectedHive).controllingPlayer);
-    		if(click == 0){
-    			if(selectedHive == -1){
-    				click = 0;
-    			} else if(currentGS.hives.get(selectedHive).controllingPlayer == GameController.Me) {
-    				click = 1;
-    				sourceHive = selectedHive;
-    				System.out.println("click: " + click + " sourceHive: " + sourceHive);
-    			}
-    		} else if (click == 2) {
-    			if(selectedHive == -1) {
-    				click = 1;
-    			} else if(currentGS.hives.get(selectedHive).controllingPlayer == GameController.Me){
-    				click = 1;
-    				sourceHive = selectedHive;	
-    				System.out.println("click: " + click + " sourceHive: " + sourceHive);
-    				//*make sourceHive a special selected color*
-    			} else { 
-    				click = 1;
-    				destHive = selectedHive;
-					try {
-						GameController.writeCurrentAttack(new Attack(GameController.Me, 
-								(char) sourceHive, (char) destHive, (short) currentGS.gameStateNum));
-						System.out.println("new attack: source: " + sourceHive + " dest: " + destHive + " stateNum: " + currentGS.gameStateNum);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-    			}
-    		}
-    	}
-    	
-    	if(!bsInput.isMouseDown(MouseEvent.BUTTON1)){
-    		if(click == 1){
-    			click = 2;
-    		}
-    	}
-    	//*tell sprites which way to face when they are walking*
+    	} //end mode 3
     	
     	//update playfield
     	playfield.update(elapsedTime);
-    	frame.update();
     }
 
     //render playfield
     public void render(Graphics2D g) {
     	playfield.render(g);
-    	for(int k = 0; k < minionNumbers.size(); k++){
-    		sf.setColor(minionNumbers.get(k).c);
-        	sf.drawString(g, minionNumbers.get(k).Minions, GameFont.CENTER, minionNumbers.get(k).x, minionNumbers.get(k).y, 
-        			sf.getWidth('1') * minionNumbers.get(k).Minions.length());
+    	if(mode == 0){
+    		//display hive wars
+    		title.drawString(g, "HiveWars", 400 - title.getWidth("HiveWars") / 2, 200 - title.getHeight());
+        	frame.render(g);
+    	} else if(mode == 2){
+    		//display waiting
+    		ann.drawString(g, "Waiting ...", 400 - ann.getWidth("Waiting ...") / 2, 300 - ann.getHeight());
+        	frame.render(g);
+    	} if(mode == 4){
+    		label.drawString(g, "IP: ", 350 - label.getWidth("IP: "), 250);
+    		label.drawString(g, "Port: ", 375 - label.getWidth("Port: "), 300);
+        	if(userError == 1){
+        		err.drawString(g, "Please enter an IP address and Port number", 
+        				400 - err.getWidth("Please enter an IP address and Port number") / 2, 500);
+        	} else if (userError == 2){
+        		err.drawString(g, "Please enter an IP address", 
+        				400 - err.getWidth("Please enter an IP address") / 2, 500);
+        	} else if (userError ==3){
+        		err.drawString(g, "Please enter a Port number", 
+        				400 - err.getWidth("Please enter a Port number") / 2, 500);
+        	}
+        	frame.render(g);
+    	} else if(mode == 6){
+        	for(int k = 0; k < minionNumbers.size(); k++){
+        		sf.setColor(minionNumbers.get(k).c);
+            	sf.drawString(g, minionNumbers.get(k).Minions, GameFont.CENTER, minionNumbers.get(k).x, 
+            			minionNumbers.get(k).y, sf.getWidth('1') * minionNumbers.get(k).Minions.length());
+        	}
     	}
-    	frame.render(g);
     }
 
     //main:
